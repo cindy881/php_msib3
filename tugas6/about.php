f6a45d3ade2c31c853f841727fc9bb2dee52a008
cindy881 <cindy.19039@mhs.unesa.ac.id>
2022-10-06 01:23:44 +0700

    Tugas 6 PHP

--- tugas6/about.php
+++ tugas6/about.php
@@ -1,19 +1,28 @@
 <section class="resume-section" id="about">
     <div class="resume-section-content">
         <h1 class="mb-0">
-            Clarence
-            <span class="text-primary">Taylor</span>
+            Cindy Viona
+            <span class="text-primary">Dzul Qurnayn</span>
         </h1>
         <div class="subheading mb-5">
-            3542 Berry Street · Cheyenne Wells, CO 80810 · (317) 585-8468 ·
-            <a href="mailto:name@email.com">name@email.com</a>
+            Tuban, Jawa Timur Indonesia · 081388293564 ·
+            <a href="mailto:name@email.com">cindydzul07@gmail.com</a>
         </div>
-        <p class="lead mb-5">I am experienced in leveraging agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.</p>
+        <p class="lead mb-5">
+            Saya adalah seorang mahasiswa aktif jurusan teknik informatika di Universitas Negeri Surabaya dengan pengalaman projek pembuatan website. Memiliki motivasi belajar tinggi untuk mengembangkan kemampuan di bidang web developer.</p>
         <div class="social-icons">
-            <a class="social-icon" href="#!"><i class="fab fa-linkedin-in"></i></a>
-            <a class="social-icon" href="#!"><i class="fab fa-github"></i></a>
-            <a class="social-icon" href="#!"><i class="fab fa-twitter"></i></a>
-            <a class="social-icon" href="#!"><i class="fab fa-facebook-f"></i></a>
+            <a class="social-icon" href="https://www.linkedin.com/in/cindydz/">
+                <i class="fab fa-linkedin-in"></i>
+            </a>
+            <a class="social-icon" href="https://github.com/cindy881">
+                <i class="fab fa-github"></i>
+            </a>
+            <a class="social-icon" href="https://twitter.com/cindydz_">
+                <i class="fab fa-twitter"></i>
+            </a>
+            <a class="social-icon" href="#!">
+                <i class="fab fa-facebook-f"></i>
+            </a>
         </div>
     </div>
 </section>
--- tugas6/study.php
+++ tugas6/study.php
@@ -1,23 +1,32 @@
 <section class="resume-section" id="education">
     <div class="resume-section-content">
-        <h2 class="mb-5">Education</h2>
+        <h2 class="mb-3">Study</h2>
+        <hr class="m-0" />
         <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
             <div class="flex-grow-1">
-                <h3 class="mb-0">University of Colorado Boulder</h3>
-                <div class="subheading mb-3">Bachelor of Science</div>
-                <div>Computer Science - Web Development Track</div>
-                <p>GPA: 3.23</p>
+                <h3 class="mb-0">Universitas Negeri Surabaya</h3>
+                <div>Teknik Informatika - Fakultas Teknik</div>
+                <p>GPA: 3.70</p>
             </div>
-            <div class="flex-shrink-0"><span class="text-primary">August 2006 - May 2010</span></div>
+            <div class="flex-shrink-0"><span class="text-primary">Agustus 2019 - Sekarang</span></div>
         </div>
-        <div class="d-flex flex-column flex-md-row justify-content-between">
+        <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
             <div class="flex-grow-1">
-                <h3 class="mb-0">James Buchanan High School</h3>
-                <div class="subheading mb-3">Technology Magnet Program</div>
-                <p>GPA: 3.56</p>
+                <h3 class="mb-0">SMA Arrohmah IBS Dau Malang</h3>
             </div>
-            <div class="flex-shrink-0"><span class="text-primary">August 2002 - May 2006</span></div>
+            <div class="flex-shrink-0"><span class="text-primary">Juli 2016 - Mei 2019</span></div>
+        </div>
+        <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+            <div class="flex-grow-1">
+                <h3 class="mb-0">SMPIT Al-Uswah Tuban</h3>
+            </div>
+            <div class="flex-shrink-0"><span class="text-primary">Juli 2013 - Mei 2016</span></div>
+        </div>
+        <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+            <div class="flex-grow-1">
+                <h3 class="mb-0">SDN Glodog</h3>
+            </div>
+            <div class="flex-shrink-0"><span class="text-primary">Juli 2007 - Mei 2013</span></div>
         </div>
     </div>
 </section>
-<hr class="m-0" />
